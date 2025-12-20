e4bd0847c7712cff636c0110e1130896dc92ca5c
parsa <parsabarqi@gmail.com>
2025-12-20 22:46:20 +0330

    async contect manager created

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from router.security_router import router as auth_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
 
